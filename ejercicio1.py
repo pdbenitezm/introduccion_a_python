f5d9b6225a5254fe7f2a4c94e53e7459b9822c1d
Pablo Benitez <103888575+pdbenitezm@users.noreply.github.com>
2023-03-01 19:38:04 -0300

    Initial commit

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+# Asignar una variable -> variable=
+# Imprimir algo en la consola -> print()
+# Tipos de input (Ingresar datos a programas)
+# int : Numérico -> variable=int()
+# input: Textual -> variable=input("Inserta algo :b")
+# Es importante tener en cuenta que siempre se obtiene con este comando (Ingresar datos a programas)
+# un valor de tipo cadena (str), por lo cual, si quisiéramos ingresar un valor
+# numérico o incluso booleano, debemos hacer uso de las conversiones de tipos de datos (int, input ...)
+print("Escribe un número")
+num_1 = int(input())
+print("Escribe otro :b")
+num_2 = int(input())
+print("Tu primer número es:", num_1, "y tu segundo número es:", num_2)
+quit(print("chao"))
