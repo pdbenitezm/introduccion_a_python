48573133ae413a6ec9f1e4e3a0de16c181dd54eb
Pablo Benitez <103888575+pdbenitezm@users.noreply.github.com>
2023-03-01 20:00:09 -0300

    nuevo

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,6 +6,7 @@
 # Es importante tener en cuenta que siempre se obtiene con este comando (Ingresar datos a programas)
 # un valor de tipo cadena (str), por lo cual, si quisiéramos ingresar un valor
 # numérico o incluso booleano, debemos hacer uso de las conversiones de tipos de datos (int, input ...)
+# 1- Declare dos números enteros y los muestre por pantalla.
 print("Escribe un número")
 num_1 = int(input())
 print("Escribe otro :b")
---
+++ ejercicio2.py
@@ -0,0 +1,3 @@
+# 2 - Declare una variable $nombre y muestre por pantalla: “Buenos dias nombre_introducido”
+$nombre = input("Cuál es tu nombre?")
+quit(print("chao"))
