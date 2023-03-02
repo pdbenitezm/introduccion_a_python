a15ec0380fc065e723d241644afef7dd43c204d3
Pablo Benitez <103888575+pdbenitezm@users.noreply.github.com>
2023-03-02 19:40:13 -0300

    nuevo

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,5 @@
 # 2 - Declare una variable $nombre y muestre por pantalla: “Buenos dias nombre_introducido”
-$nombre = input("Cuál es tu nombre?")
-quit(print("chao"))
+print("Introduzca su nombre")
+nombre_introducido = input()
+print("Buenos días", nombre_introducido, ":)")
+quit(print("Fin código"))
---
+++ ejercicio3.py
@@ -0,0 +1,7 @@
+# Definir un número y obtiene y muestra por pantalla el doble y el triple de ese número.
+print("Escribe un número")
+numero = int(input())
+numero_doble = numero ** 2
+numero_triple = numero ** 3
+print("El doble de tu número es:", numero_doble, "Y el triple es:", numero_triple)
+quit(print("Fin código"))
